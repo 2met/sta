7344c435070be04c19a454c2e42e01adb3723f58
2met <118592756+2met@users.noreply.github.com>
2022-11-29 03:48:59 -0800

    Update tab.js

--- static/cloak/tab.js
+++ static/cloak/tab.js
@@ -11,7 +11,7 @@ const changeTabTitle = () => {
   const newtitle = document.getElementById("userinput");
   if (newtitle.value == ""){ //check if the input is blank when they submit
       window.localStorage.removeItem("title");
-      window.document.title = "Amazon Lake - Q&As"
+      window.document.title = "Amazon River - Q&As"
       document.getElementById("console-output").style.color = "red"; //error = red
       console_output.innerText = "No title entered. Default applied" //return output successful
   } else {
