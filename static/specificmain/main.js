71fb345c1f7a8667d2b6a3cf92603b094eb31dbe
2met <118592756+2met@users.noreply.github.com>
2022-12-10 19:22:25 -0800

    Update main.js

--- static/specificmain/main.js
+++ static/specificmain/main.js
@@ -22,21 +22,20 @@ tab("Loading Tab Cloak...");
 //Declare variables for cloak here
 const local_title = localStorage.getItem("title");
 const local_icon = localStorage.getItem("icon");
-console.log(local_icon);
 
 // Set tab title (if needed)
 if (window.localStorage.hasOwnProperty("title")) {
   document.title = local_title;
-  tab("Title set to: " + local_title);
+  console.log("Title set to: " + local_title);
 } else {
-  tab("Title not set :(");
+  console.log("Title not set :(");
 }
 // Set tab icon (if needed)
 if (window.localStorage.hasOwnProperty("icon")) {
   document.querySelector("link[rel=icon]").href = local_icon;
-  tab("Icon set to: " + local_icon);
+  console.log("Icon set to: " + local_icon);
 } else {
-  tab("Icon not set :(");
+  console.log("Icon not set :(");
 }
 tab("Tab cloak settings can be found in localStorage! If you want to change them, you can go tab cloak page.");
 console.groupEnd();
