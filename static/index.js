e348c71d7e48f163caddacdcd5773f397797cc09
2met <118592756+2met@users.noreply.github.com>
2022-12-23 07:01:29 -0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1,11 +1,43 @@
 const form = document.querySelector('form');
-const input = document.querySelector('input');
+const input = document.querySelector('.form__input');
+const gobtn = document.querySelector('.submit');
 
-document.body.addEventListener("keydown", function (event) {
-    if (event.key === ",") {
-        window.location.replace("https://google.com");
-    }
+
+
+
+
+
+gobtn.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+        var urle = "https://" + document.domain + __uv$config.prefix + __uv$config.encodeUrl(url);
+        if (urle) {
+            var win; {
+                if (win) { win.focus(); } else {
+                    win = window.open();
+                    win.document.body.style.margin = '0';
+                    win.document.body.style.height = '100vh';
+                    var iframe = win.document.createElement('iframe');
+                    iframe.style.border = 'none';
+                    iframe.style.width = '100%';
+                    iframe.style.height = '100%';
+                    iframe.style.margin = '0';
+                    iframe.src = urle;
+                    win.document.body.appendChild(iframe)
+                }
+            }
+        }
+    });
+
 });
+
+
+
 
 form.addEventListener('submit', async event => {
     event.preventDefault();
@@ -21,22 +53,7 @@ form.addEventListener('submit', async event => {
     });
 });
 
-function isUrl(val = ''){
+function isUrl(val = '') {
     if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
     return false;
 };
-
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.get("url") != null) {
-    document.getElementsByTagName("input")[0].value = urlParams.get("url");
-    window.navigator.serviceWorker.register(__uv$config.sw, {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
-}
